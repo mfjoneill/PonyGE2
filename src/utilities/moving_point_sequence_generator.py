fe5abec1f6c526fead637ed3a3add5f5a9967cb8
dvpfagan <fagan.david@gmail.com>
2016-10-13 14:32:37 +0100

    Added euclidean distances for steps and put files in datasets

--- src/utilities/moving_point_sequence_generator.py
+++ src/utilities/moving_point_sequence_generator.py
@@ -1,11 +1,14 @@
 import random
+from scipy.spatial import distance
 
 def generate_sequence(name, max_p=10000,interval=None, seed=None):
     points = []
+    dists = []
     if seed is None:
         random.seed(12345)
     else:
         random.seed(seed)
+    prev_point = (0,0,0)
     while len(points)<max_p:
         if interval is None:
             gard = max_p - len(points)
@@ -16,9 +19,13 @@ def generate_sequence(name, max_p=10000,interval=None, seed=None):
             if seq_len > gard:
                 for i in range(gard):
                     points.append(new_point)
+                    dists.append(distance.euclidean(prev_point,new_point))
+                    prev_point = new_point
             else:
                 for i in range(seq_len):
                     points.append(new_point)
+                    dists.append(distance.euclidean(prev_point, new_point))
+                    prev_point = new_point
         elif interval > 1:
             gard = max_p - len(points)
             new_point = (random.randint(0, 10000),
@@ -27,22 +34,32 @@ def generate_sequence(name, max_p=10000,interval=None, seed=None):
             if interval > gard:
                 for i in range(gard):
                     points.append(new_point)
+                    dists.append(distance.euclidean(prev_point, new_point))
+                    prev_point = new_point
             else:
                 for i in range(interval):
                     points.append(new_point)
+                    dists.append(distance.euclidean(prev_point, new_point))
+                    prev_point = new_point
         elif interval==1:
-            points.append((random.randint(0, 10000),
+            new_point = (random.randint(0, 10000),
                            random.randint(0, 10000),
-                           random.randint(0, 10000)))
+                           random.randint(0, 10000))
+            points.append(new_point)
+            dists.append(distance.euclidean(prev_point, new_point))
+            prev_point = new_point
         else:
             print("Specify an interval greater than 0 "
                   "or else provide no interval for random interval")
             exit()
 
     f = open(name+'.csv', 'w')
-    for point in points:
-        f.write("%d,%d,%d\n" % point)
+    g = open(name+'_dists.csv','w')
+    for i in range(len(points)):
+        f.write("%d,%d,%d\n" % points[i])
+        g.write("%d\n"% dists[i])
     f.close()
+    g.close()
 
 def read_target_points(filename):
     points = []
@@ -55,8 +72,8 @@ def read_target_points(filename):
 
 if __name__ == "__main__":
     print("Uncomment as needed")
-    #generate_sequence('random')
-    #generate_sequence('10_steps',interval=10)
+    generate_sequence('random')
+    generate_sequence('10_steps',interval=10)
     generate_sequence('20_steps',interval=20)
-    #generate_sequence('1_step',interval=1)
+    generate_sequence('1_step',interval=1)
     #read_target_points('1_step.csv')
